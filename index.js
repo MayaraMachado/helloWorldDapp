ac7244b8a7d8bd99ce42dfa1e44e4e9157501d33
Mayara Machado <maaih18@gmail.com>
2018-04-03 16:28:50 -0300

    Correção na conexão com web3

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-
-//https://github.com/mneedham/ethereum-nursery/blob/master/eth_solc.js
-const fs = require("fs"),
-      abiDecoder = require('abi-decoder'),
-      Web3 = require('web3'),
-	  solc = require('solc');
-
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545");
-const code = fs.readFileSync('hello.sol').toString();
-const compiledCode = solc.compile(code);
-
-const abi = JSON.parse(compiledCode.contracts[':Hello'].interface);
-const abiInterface = compiledCode.contracts[':Hello'].interface;
-
-//const HelloContract = web3.eth.contract(abiInterface);
-const HelloContract = web3.eth.contract(abi)
-const bytecode = compiledCode.contracts[':Hello'].bytecode;
-
-
-const deployedContract = HelloContract.new('World',{data: byteCode, from: web3.eth.accounts[0], gas: 4700000})
-const address = deployedContract.address
-const contractInstance = HelloContract.at(deployedContract.address)
-
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+abi= JSON.parse('[{"constant":true,"inputs":[],"name":"getNome","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_novoNome","type":"string"}],"name":"setNome","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_start","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]');
+HelloContract = web3.eth.contract(abi);
+web3.eth.defaultAccount = web3.eth.accounts[0] 
+contractInstance = HelloContract.at('0x9b51edd5b5ed1af5826e4b0fdcde091a30c12fe8');
 console.log(contractInstance);
 
 /*
@@ -32,14 +14,13 @@ function setValue(){
 function setValue(){
 	var nome = document.getElementById('nomeInput').value;
 	contractInstance.setNome(nome);
-	location.reload()
+	location.reload();
 }
 
 contractInstance.getNome(function(err, result){
             if(!err)
                 {
                     $("#nome").html(result);
-                    console.log(result);
                 }
             else
                 console.error(err);
